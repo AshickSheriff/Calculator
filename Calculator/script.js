37c061a874660787db377c01d646f4f25e392568
Ashick Sheriff <105576514+AshickSheriff@users.noreply.github.com>
2023-07-18 12:18:11 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,23 @@
+var os=document.getElementById("opscreen"); 
+
+function display(num){
+    os.value += num; 
+}
+
+function calculate(){
+    try{
+        os.value = eval(os.value);
+    }
+    catch(err){
+        alert("Invalid");
+    }
+}
+
+function clr(){
+    console.log("hello");
+    os.value = "";
+}
+
+function del(){
+    os.value = os.value.slice(0,-1);
+}
